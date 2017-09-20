9521bbe4eebbe266cd78c8e9c9cd204de82befdf
CIA <CIA@CIA.CIA>
2017-09-20 13:41:17 +0200

    movie table click

--- src/sample/Controller/AddMovieController.java
+++ src/sample/Controller/AddMovieController.java
@@ -111,4 +111,5 @@ public class AddMovieController implements Initializable {
         loadMoviesFromDB();
     }
 
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/sample/Views/first.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hello Bogdan");
         primaryStage.setScene(new Scene(root,700,500));
         primaryStage.show();
     }
